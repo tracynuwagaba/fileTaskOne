45b7e25a60405b214b30f1fc09f75aebf722f007
TracyNuwagaba <tnuwagaba191@gmail.com>
2021-04-18 04:58:26 +0300

    check if the result folder exists

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const fs = require('fs');
 const fetch = require('node-fetch');
 
+// check if the result folder exists
+fs.access('./result', (err) => {
+    if (err) {
+        console.log('Folder does not exist');
+    } else {
+        console.log('Folder exists!');
+    }
+});
+
 // GET request to fetch data from the API using fetch
 fetch('http://jsonplaceholder.typicode.com/posts')
     .then(response => response.text())
     .then(data => {
+        // write the JSON data from the API to posts.json
         fs.writeFile('./result/posts.json', data, (err) => {
             if (err) {
                 console.log(err);
